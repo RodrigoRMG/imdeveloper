454e4edfac042e78be1c7010b221aaa3db5addfb
rodrigo <rodrigo_2392@hotmail.com>
2019-10-03 03:44:18 -0500

    fix problemas con links y scroll

--- src/components/Acerca.js
+++ src/components/Acerca.js
@@ -8,7 +8,7 @@ export default (props) => {
             <p className="comment"><span className="codeWhite ml-2 invisible md:visible">3</span>que integra react y graqhql para el desarrollo de aplicaciones y sitios web</p>
             <p className="comment"><span className="codeWhite ml-2 invisible md:visible">4</span>de manera más rápida y eficiente</p>
             <p className="comment"><span className="codeWhite ml-2 invisible md:visible">5</span>Si quieres ver el código puedes ir al siguiente link:</p>
-            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">6</span><a href="https://github.com/RodrigoRMG/imdeveloper" target="blank">https://github.com/RodrigoRMG/imdeveloper</a></p>
+            <p className="codeBlue" style={{color:"#FFF"}}><span className="codeWhite ml-2 invisible md:visible">6</span><a href="https://github.com/RodrigoRMG/imdeveloper" target="blank">Repositorio de github</a></p>
             <p className="comment"><span className="codeWhite ml-2 invisible md:visible">7</span> */</p>
             <p>
                 <div className="inline-block">
--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -21,16 +21,14 @@ export default (props) => {
         <div className="pl-4 pt-4" >
 
             <p className="comment"><span className="codeWhite invisible ml-2 md:visible">1</span>/*</p>
-            <p className="comment max-w-full overflow-x-scroll"><span className="codeWhite ml-2 invisible md:visible">2</span>Puedes visitar mi perfil de linkedin:</p>
-            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">3</span><a href="https://www.linkedin.com/in/rodrigo-m%C3%A9ndez-8808aab1/">https://www.linkedin.com/in/rodrigo-m%C3%A9ndez-8808aab1/</a></p>
-            <span className="codeWhite ml-2 invisible md:visible">4</span>
-            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">5</span>O también puedes descargar mi CV:</p>
-            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">6</span><a href="https://www.dropbox.com/s/up09xyqsnzj29pz/Rodrigo%20Mendez.pdf?dl=0" target="blank">https://www.dropbox.com/s/up09xyqsnzj29pz/Rodrigo%20Mendez.pdf?dl=0</a></p>
-            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">7</span> */</p>
+            <p className="comment "><span className="codeWhite ml-2 invisible md:visible">2</span><a href="https://www.linkedin.com/in/rodrigo-m%C3%A9ndez-8808aab1/">Puedes visitar mi perfil de linkedin</a></p>
+            <span className="codeWhite invisible ml-2 md:visible">3</span>
+            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">4</span><a href="https://www.dropbox.com/s/up09xyqsnzj29pz/Rodrigo%20Mendez.pdf?dl=0" target="blank">O también puedes descargar mi CV</a></p>
+            <p className="comment"><span className="codeWhite ml-2 invisible md:visible">5</span> */</p>
 
             <p>
                 <div className="inline-block">
-                    <span className="codePurple"><span className="codeWhite ml-2 invisible md:visible">8</span>import </span>
+                    <span className="codePurple"><span className="codeWhite ml-2 invisible md:visible">6</span>import </span>
                     <span className="codeBlue"> React</span>
                     <span className="codePurple"> from </span>
                     <span className="codeOrange"> "react"</span>
@@ -39,11 +37,11 @@ export default (props) => {
 
            
          
-            <div>
-            <span className="codeWhite invisible ml-2 md:visible">9</span>
+
+            
             <p>
                 <div className="inline-block">
-                        <span className="codePurple"><span className="codeWhite invisible md:visible">10</span>export </span>
+                        <span className="codePurple"><span className="codeWhite ml-2  invisible md:visible">7</span>export </span>
                         <span className="codePurple">default </span>
                         <span className="codeGray"> ( ) </span>
                         <span className="codeBlue"> => </span>
@@ -53,13 +51,13 @@ export default (props) => {
                 
             <p>
                 <div className="inline-block">
-                    <span className="codeBlue"> <span className="codeWhite invisible md:visible">11</span> {"<ul>"}</span>
+                    <span className="codeBlue"> <span className="codeWhite ml-2 invisible md:visible">8</span> {"<ul>"}</span>
                 </div>
             </p>
             <p>
                 <div className="inline-block">
                     <span className="codeBlue"> 
-                        <span className={`codeWhite invisible md:visible`}>12</span> 
+                        <span className={`codeWhite invisible ml-2 md:visible`}>9</span> 
                         <span className="pl-8">{"<li>"}</span>
                         <span className="codeGray pl-0 pr-0" style={{paddingLeft:0,paddingRight:0}}>Teléfono: 3321214680</span>
                         <span>{"</li>"}</span>
@@ -70,7 +68,7 @@ export default (props) => {
             <p>
                 <div className="inline-block">
                     <span className="codeBlue"> 
-                        <span className={`codeWhite invisible  md:visible`}>13</span> 
+                        <span className={`codeWhite invisible  md:visible`}>10</span> 
                         <span className="pl-8">{"<li>"}</span>
                         <span className="codeGray pl-0 pr-0" style={{paddingLeft:0,paddingRight:0}}>Email: rodrigo_2392@hotmail.com</span>
                         <span>{"</li>"}</span>
@@ -80,17 +78,16 @@ export default (props) => {
             
             <p>
                 <div className="inline-block">
-                    <span className="codeBlue"> <span className="codeWhite invisible  md:visible">14</span> {"<ul>"}</span>
+                    <span className="codeBlue"> <span className="codeWhite invisible  md:visible">11</span> {"<ul>"}</span>
                 </div>
             </p>
                 
             <p>
                 <div className="inline-block">
-                <span className="codeBlue" style={{color:"#FFF"}}><span className="codeWhite invisible md:visible">15</span> ) </span>
+                <span className="codeBlue" style={{color:"#FFF"}}><span className="codeWhite invisible md:visible">12</span> ) </span>
                 </div>
                
             </p>
-            </div>
         </div>
     )
 }
