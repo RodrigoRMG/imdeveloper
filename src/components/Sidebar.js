c9c7f51113461253d0f3a9629e1a05d4d899fe55
rodrigo <rodrigo_2392@hotmail.com>
2019-10-02 20:35:25 -0500

    fix en la barra principal de las tabs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,12 +7,12 @@ export default () => {
             <li className="mt-1">
                 <a className="py-2 px-4 font-semibold text-white" onClick={()=>setExpanded(!expanded)}>
                     {
-                        !expanded?<i className="chevron bottom "></i>:<i className="chevron right"></i>
+                        expanded?<i className="chevron bottom "></i>:<i className="chevron right"></i>
                     }
                     <span className="pl-4"> Rodrigo Mendez</span>
                     </a>
             </li>
-            <div className={expanded?"hidden":""}>
+            <div className={!expanded?"hidden":""}>
                 <li className="mt-1 pl-8">
                     <a className="py-2 px-4  text-white" href="#">
                         <span className="text-yellow-400 font-bold">JS</span> index.js
